807b947d686324196bb3ed765e527bee136abfbc
aparra001 <61757290+aparra001@users.noreply.github.com>
2020-10-15 10:46:21 +0000

    Update run.example.sh

--- scripts/run.example.sh
+++ scripts/run.example.sh
@@ -5,4 +5,4 @@ export ETH_WALLET=0xB71E12CF3A8dA259FF191f0AD234FA46eEb88b72
 export WORKER_NAME=aws
 
 # Start mining!
-docker run --gpus all -e 0xB71E12CF3A8dA259FF191f0AD234FA46eEb88b72 -e aws -P -it ethminer:latest
+docker run --gpus all -e ETH_WALLET -e WORKER_NAME -P -it ethminer:latest
